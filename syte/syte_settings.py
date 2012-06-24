1783891ab18877f01985e1cb45b42030dad9ae69
Owen Williams <owen@owened.co.nz>
2012-06-25 10:57:52 +1200

    hello

--- syte/syte_settings.py
+++ syte/syte_settings.py
@@ -1,5 +1,5 @@
 
-DEPLOYMENT_MODE = 'prod'
+DEPLOYMENT_MODE = 'dev'
 COMPRESS_REVISION_NUMBER = '1.0'
 
 #Blog Integration: Tumblr
@@ -60,11 +60,11 @@ DISQUS_SHORTNAME = ''
 
 
 
-if DEPLOYMENT_MODE == 'prod':
+if DEPLOYMENT_MODE == 'dev':
     SITE_ROOT_URI = 'http://127.0.0.1:8000/'
     DEBUG = True
 else:
     DEBUG = False
-    SITE_ROOT_URI = 'http://warm-dusk-4419.herokuapp.com/'
+    SITE_ROOT_URI = '[ENTER PROD URL HERE] ex. http://rigoneri.herokuapp.com/'
 
 MEDIA_URL = SITE_ROOT_URI + 'static/'
